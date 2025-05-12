39d916198e96d759d92c1386e308a61adf12580c
Diego005Dev <metallica3999@gmail.com>
2025-05-11 23:11:33 -0600

    Delete web-portfolio directory

--- web-portfolio/app/page.tsx
+++
@@ -1,8 +0,0 @@
-import { redirect } from "next/navigation"
-
-// For static export, we can't use headers() to detect the browser language
-// Instead, we'll redirect to the English version by default
-export default function RootPage() {
-  // Redirect to the English version by default
-  redirect("/en")
-}
