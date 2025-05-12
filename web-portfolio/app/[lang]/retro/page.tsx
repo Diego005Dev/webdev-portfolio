bb5efea1379d40399c874c81e1a79533dbd9fd81
Diego005Dev <metallica3999@gmail.com>
2025-05-11 22:45:32 -0600

    Delete web-portfolio directory

--- web-portfolio/app/[lang]/retro/page.tsx
+++
@@ -1,8 +0,0 @@
-import { getDictionary } from "@/i18n"
-import { ClientHome } from "@/components/client-home"
-
-export default async function RetroPage({ params }: { params: { lang: string } }) {
-  const dict = await getDictionary(params.lang)
-
-  return <ClientHome lang={params.lang} dictionary={dict} />
-}
